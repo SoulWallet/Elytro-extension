5dbd5a094c7bdd3b091917cbb4311c426bd92cbf
zewei.zhang <zewei.zhang@soulwallet.io>
2024-12-26 10:49:33 +0800

    feat: add icons

--- apps/extension/src/entries/side-panel/pages/Settings.tsx
+++ apps/extension/src/entries/side-panel/pages/Settings.tsx
@@ -3,6 +3,14 @@ import SecondaryPageWrapper from '../components/SecondaryPageWrapper';
 import { Button } from '@/components/ui/button';
 import { SIDE_PANEL_ROUTE_PATHS } from '../routes';
 import { navigateTo, SidePanelRoutePath } from '@/utils/navigation';
+import {
+  ExternalLinkIcon,
+  LayoutGridIcon,
+  LockKeyholeIcon,
+  SettingsIcon,
+  ShieldIcon,
+  UserRoundIcon,
+} from 'lucide-react';
 
 export default function Settings() {
   const { lock } = useKeyring();
@@ -19,9 +27,13 @@ export default function Settings() {
       footer={
         <div className="flex flex-col space-y-2 w-full">
           <Button variant="secondary" onClick={handleLock}>
-            Lock Elytro
+            <LockKeyholeIcon />
+            <span className="ml-sm">Lock Elytro</span>
           </Button>
-          <Button variant="outline">FAQ</Button>
+          <Button variant="outline">
+            <ExternalLinkIcon />
+            <span className="ml-sm">FAQ</span>
+          </Button>
           <div className="text-center text-gray-750">
             <p>Join Telegram group</p>
             <p>Version 1.00. Third party software licenses</p>
@@ -34,8 +46,14 @@ export default function Settings() {
           Account settings
         </h2>
         <div className="space-y-2">
-          <div className="elytro-setting-item">Social recovery</div>
-          <div className="elytro-setting-item">Connected apps</div>
+          <div className="elytro-setting-item">
+            <ShieldIcon />
+            <span className="ml-sm">Social recovery</span>
+          </div>
+          <div className="elytro-setting-item">
+            <LayoutGridIcon />
+            <span className="ml-sm">Connected apps</span>
+          </div>
         </div>
       </div>
       <div className="mb-4">
@@ -45,9 +63,13 @@ export default function Settings() {
             onClick={() => handleJumpPage(SIDE_PANEL_ROUTE_PATHS.LocalProfile)}
             className="elytro-setting-item"
           >
-            Local Profile
+            <UserRoundIcon />
+            <span className="ml-sm">Local Profile</span>
           </div>
-          <div className="elytro-setting-item">Network configuration</div>
+          <div className="elytro-setting-item">
+            <SettingsIcon />
+            <span className="ml-sm">Network configuration</span>
+          </div>
         </div>
       </div>
     </SecondaryPageWrapper>
